fc427ee579966b60b241a0d0829ca1599efdd619
Didier TAHOBEU <didier@collaborationcapital.tech>
2025-05-22 07:59:04 +0200

    feat: TP5 ajout de children dans recipCard

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ function App() {
             title={recipe.title}
             image={recipe.image}
             ingredients={recipe.ingredients}
-          />
+            >
+            <button>Voir la recette</button>
+          </RecipeCard>
         ))}
       </div>
       <Footer />
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,17 +1,21 @@
 import PropTypes from 'prop-types';
 import { formatTitle } from '../utils/formatter';
 
-function RecipeCard({ title, image, ingredients }) {
+function RecipeCard({title, image, ingredients, children }) {
   return (
     <div className="recipe-card" style={{ border: '1px solid #ccc', borderRadius: '8px', padding: '16px', maxWidth: '300px' }}>
       <img src={image} alt={formatTitle(title)} style={{ width: '100%', borderRadius: '8px' }} />
       <h2>{formatTitle(title)}</h2>
       <h3>Ingredients ({ingredients.length}):</h3>
-      <ul>
-        {ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
+        <ul>
+          {ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+        <div style={{display: 'flex', justifyContent: 'end'}} className="recipe-card__footer">
+            {children}
+        </div>
+
     </div>
   );
 }
@@ -21,4 +25,5 @@ RecipeCard.propTypes = {
   title: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+  children: PropTypes.node,
 };
